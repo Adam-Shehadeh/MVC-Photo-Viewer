9cbe6ef92238158ab1d099341cf6f9f58c551611
Shehadeh <ashehadeh@DWFP.com>
2018-06-07 14:42:54 -0500

    Initial Commit
    Simple example of photoviewer without using libraries

---
+++ MVC Photo Viewer/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿//Author:     Adam Shehadeh
+//Date:       6/7/2018
+//Desc:       MVC photo viewer taking images from a source, converting them into a byte string, and sending them as array to view.
+//            The view then allows us to see these photos.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC_Photo_Viewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<string> ImageTitleList = new List<string>() {
+                "1074.jpg",
+                "1111.jpg",
+                "B95.png",
+                "_AFW.jpg"
+            };
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.ImageTitleList = ImageTitleList;
+            return View();
+        }
+        public JsonResult GetImageData(int index) {
+            return Json(PhotoService.getPhotoData(ImageTitleList[index]), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult TestServer(string d1) {
+            return Json(d1 + " and now from server.", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVC Photo Viewer/Controllers/PhotoService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Drawing;
+
+namespace MVC_Photo_Viewer.Controllers {
+    public static class PhotoService {
+
+        private static readonly string ROOT = "C:\\Users\\AShehadeh\\D&W Fine Pack\\Marketing Communications - Product Photos\\";
+        //Takes list of string file names. Example: 1074.jpg, 1111.jpg, B95.png
+        //Returns list of byte strings for photos, sent to view
+        public static string getPhotoData(string name) {
+            string local = string.Empty;    //Will propogate with return values
+            try {
+                string fullpath = ROOT + name;
+                Image img = Image.FromFile(fullpath);
+                ImageConverter ic = new ImageConverter();
+                byte[] xByte = (byte[])ic.ConvertTo(img, typeof(byte[]));
+                local = string.Format("data:image/jpg;base64,{0}", Convert.ToBase64String(xByte));
+            } catch { }
+            
+            return local;
+        }
+    }
+}
